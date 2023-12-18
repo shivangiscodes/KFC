dce2d691fd383e76d9b8f5c9cbf1ba0aa9e7b5a7
shivangiscodes <143073166+shivangiscodes@users.noreply.github.com>
2023-12-18 12:05:51 +0530

    Add files via upload

---
+++ scripts/blog.js
@@ -0,0 +1,32 @@
+// Add your JavaScript code here
+
+// Example: Log a message to the console when a product post is clicked
+document.getElementById('chizza-post').addEventListener('click', function() {
+    console.log('Clicked on #CHIZZa post!');
+  });
+
+document.getElementById('chicken-post').addEventListener('click', function() {
+    console.log('Clicked on #CHIZZa post!');
+  });
+
+document.getElementById('biryani-post').addEventListener('click', function() {
+    console.log('Clicked on #CHIZZa post!');
+  });
+
+document.getElementById('box-post-post').addEventListener('click', function() {
+    console.log('Clicked on #CHIZZa post!');
+  });
+
+document.getElementById('burger-post').addEventListener('click', function() {
+    console.log('Clicked on #CHIZZa post!');
+  });
+
+document.getElementById('snacks-post').addEventListener('click', function() {
+    console.log('Clicked on #CHIZZa post!');
+  });
+
+document.getElementById('beverage-post').addEventListener('click', function() {
+    console.log('Clicked on #CHIZZa post!');
+  });
+  
+  
